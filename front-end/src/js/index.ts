56b9f9c203d498952c2864dfdb8e094857672978
Daniel Hoop <daniel.hoop@gmx.net>
2020-10-04 17:50:44 +0200

    front-end: websocket.

--- front-end/src/js/index.ts
+++ front-end/src/js/index.ts
@@ -1,7 +1,23 @@
 import game from "./game";
 import jQuery from "./jquery-2.1.3.min";
 
+const websocketGame = {
+    socket: null,
+};
+
 (function($) {
+    // Start the game.
     $(document).ready(game);
-// eslint-disable-next-line no-undef
+
+    if (window["WebSocket"]) {
+        const socket = new WebSocket("ws://localhost:8000");
+        socket.onopen = function(e) {
+            console.log("Websocket connection established.");
+        };
+        socket.onclose = function(e) {
+            console.log("Websocket connection closed.");
+        };
+
+        websocketGame.socket = socket;
+    }
 })(jQuery);
